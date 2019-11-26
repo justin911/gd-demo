7eb1e6b0237092d45db73906dfb2750d131e8506
li912 <767849912@qq.com>
2019-11-26 21:46:40 +0800

    feat: add modules

---
+++ demo-order-service/src/main/java/com/example/demo/order/service/DemoOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.order.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoOrderServiceApplication.class, args);
+    }
+
+}
