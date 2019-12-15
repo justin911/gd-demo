c2767bc3cac58ff1dcfc6ff503fdd052cdee12ba
li912 <767849912@qq.com>
2019-12-15 21:03:47 +0800

    feat: 添加consul配置

--- demo-order-service/src/main/java/com/example/demo/order/service/DemoOrderServiceApplication.java
+++ demo-order-service/src/main/java/com/example/demo/order/service/DemoOrderServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.demo.order.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DemoOrderServiceApplication {
 
     public static void main(String[] args) {
