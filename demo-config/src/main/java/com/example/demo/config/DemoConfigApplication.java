56436d56f6471c5a9ac3744346b1b4ca7b244a68
li912 <767849912@qq.com>
2019-12-15 20:49:57 +0800

    feat: 添加consul配置

--- demo-config/src/main/java/com/example/demo/config/DemoConfigApplication.java
+++ demo-config/src/main/java/com/example/demo/config/DemoConfigApplication.java
@@ -2,8 +2,10 @@ package com.example.demo.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DemoConfigApplication {
 
     public static void main(String[] args) {
